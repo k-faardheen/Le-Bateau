2ea66dc843c9cb1e8873475df9255f0a08ddf64e
k-faardheen <k.faardheen@gmail.com>
2022-03-07 20:46:38 +0400

    initial commit

--- script/log-in.js
+++ script/log-in.js
@@ -6,9 +6,11 @@ let validateBlank = ()=>{
     let pass = document.getElementById("password").value.trim()
 
     if (email.length ===0){
+        alert("You need to enter your username!")
         return false
     }
     if (pass.length ===0){
+        alert("You need to enter a valid Password!")
         return false
     }
     return true
