0aa7658981de61020ce1d7231fb54aa70bad1b50
k-faardheen <k.faardheen@gmail.com>
2022-03-08 12:29:12 +0400

    initial commit

--- js-modules/script/validate js-answer.js
+++ js-modules/script/validate js-answer.js
@@ -38,21 +38,19 @@ getAnswer().then(data => {
 submitBtns.forEach(btn => { 
     btn.addEventListener('click', (e) => { 
         e.preventDefault();
-        const root = e.target.getRootNode(); 
-        const getId = root.querySelector('.header > .header-wrapper').id
         const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
-        const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form
+        const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form (Where each input fields are stored in a nodeList)
         const codeSnippetWrapper = parent.querySelector('.code-snippet-wrapper'); //Retrieve the wrapper to change the contents(i.e if answer is wrong or correct)  
         let correct; 
             
-        if(e.target.className === 'try-again') { 
-            tryAgain(e.target, parent); 
-            showHTMLElements(e.target.parentNode.childNodes[5]);
+        if(e.target.className === 'try-again') { // Check if the button was already clicked before. If so, we should allow the user to try again. 
+            tryAgain(e.target, parent); // Fire the function "tryAgain". Takes 2 arguments (the submit button, and the form)
+            showHTMLElements(e.target.parentNode.childNodes[5]); // Show the "Show Answer" button. 
         }else { 
-            for(i = 0; i < inputBox.length; i++) { 
+            for(i = 0; i < inputBox.length; i++) { // Cycle through all the input fields in the form 
                 correct = true; 
                 if(inputBox[i].value !== val[parent.id][i]){ //Check input value of user against answer in the JSON file. 
-                    correct = false;  // If one answer is wrong, set flag to FALSE and break from the loop. 
+                    correct = false;  // If one answer is wrong, set flag to FALSE and BREAK from the loop. 
                     break; 
                 }
             }
@@ -72,21 +70,21 @@ submitBtns.forEach(btn => {
 showAnsBtns.forEach(btn => { 
     btn.addEventListener('click', e => {    
         e.preventDefault(); 
-        e.target.classList.toggle('clicked'); 
+        e.target.classList.toggle('clicked'); // The class of the button will toggle. That is, it will either be "" or clicked.  
         const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
         const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form
 
-        if(e.target.className !== '') { 
-            e.target.innerHTML = 'Hide Answer';
-            hideHTMLElements(e.target.parentNode.childNodes[1]);
+        if(e.target.className !== '') { // Checks if the user clicked on the button. If so, 
+            e.target.innerHTML = 'Hide Answer'; // Change the text inside the button from "Show Answer" to "Hide Answer"
+            hideHTMLElements(e.target.parentNode.childNodes[1]); //Hide "Submit Answer" button. 
             for(i = 0; i < inputBox.length; i++) { 
-                inputBox[i].value = val[parent.id][i];  
+                inputBox[i].value = val[parent.id][i]; // Insert answers from JSON file to the input fields. 
             }
-        }else { 
-            showHTMLElements(e.target.parentNode.childNodes[1]);
-            e.target.innerHTML = 'Show Answer'; 
+        }else { // If not clicked, 
+            showHTMLElements(e.target.parentNode.childNodes[1]); //Display "Submit Answer" button. 
+            e.target.innerHTML = 'Show Answer'; // Change text of the button from "Hide Answer" to "Show Answer". 
             for(j = 0; j < inputBox.length; j++) { 
-                inputBox[j].value = '';  
+                inputBox[j].value = '';  // Reset all input field to blank. 
             }
         }
     })
