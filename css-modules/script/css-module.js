04bd7603630cfc170125c506209995eda7d8a354
k-faardheen <k.faardheen@gmail.com>
2022-03-08 15:14:44 +0400

    initial commit

--- css-modules/script/css-module.js
+++ css-modules/script/css-module.js
@@ -16,31 +16,31 @@ getAnswer().then(data => {
 });
 
 //Event Listeners
-
 submitBtns.forEach(btn => { 
     btn.addEventListener('click', (e) => { 
         e.preventDefault();
         const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
-        const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form
-        const codeSnippetWrapper = parent.querySelector('.code-snippet-wrapper'); //Retrieve the wrapper to change the contents(i.e if answer is wrong or correct) 
-        let flag; 
-        if(e.target.className === 'try-again') { 
-            tryAgain(e.target, parent); 
-            showHTMLElements(e.target.parentNode.childNodes[5]);
+        const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form (Where each input fields are stored in a nodeList)
+        const codeSnippetWrapper = parent.querySelector('.code-snippet-wrapper'); //Retrieve the wrapper to change the contents(i.e if answer is wrong or correct)  
+        let correct; 
+            
+        if(e.target.className === 'try-again') { // Check if the button was already clicked before. If so, we should allow the user to try again. 
+            tryAgain(e.target, parent); // Fire the function "tryAgain". Takes 2 arguments (the submit button, and the form)
+            showHTMLElements(e.target.parentNode.childNodes[5]); // Show the "Show Answer" button. 
         }else { 
-            for(i = 0; i < inputBox.length; i++) { 
-                flag = true; 
-                if(inputBox[i].value.toLowerCase() !== val[parent.id][i]){ //Check input value of user against answer in the JSON file. 
-                    flag = false;  //Sets the a flag to TRUE if each input values are correct. 
-                    break;
+            for(i = 0; i < inputBox.length; i++) { // Cycle through all the input fields in the form 
+                correct = true; 
+                if(inputBox[i].value !== val[parent.id][i]){ //Check input value of user against answer in the JSON file. 
+                    correct = false;  // If one answer is wrong, set flag to FALSE and BREAK from the loop. 
+                    break; 
                 }
             }
         
-            if(flag){ 
-                //CALL ON THE FUNCTION changeHTMLElements. 
-                changeHTMLElements(e.target, codeSnippetWrapper, flag); 
+            if(correct){ 
+                //CALL ON THE FUNCTION changeHTMLElements to show appropriate template (i.e --> either wrong template OR correct template). 
+                changeHTMLElements(e.target, codeSnippetWrapper, correct); 
             }else {
-                changeHTMLElements(e.target, codeSnippetWrapper, flag)
+                changeHTMLElements(e.target, codeSnippetWrapper, correct)
             }
         }
 
@@ -51,27 +51,28 @@ submitBtns.forEach(btn => {
 showAnsBtns.forEach(btn => { 
     btn.addEventListener('click', e => {    
         e.preventDefault(); 
-        e.target.classList.toggle('clicked'); 
+        e.target.classList.toggle('clicked'); // The class of the button will toggle. That is, it will either be "" or clicked.  
         const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
         const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form
 
-        if(e.target.className !== '') { 
-            e.target.innerHTML = 'Hide Answer';
-            hideHTMLElements(e.target.parentNode.childNodes[1]);
+        if(e.target.className !== '') { // Checks if the user clicked on the button. If so, 
+            e.target.innerHTML = 'Hide Answer'; // Change the text inside the button from "Show Answer" to "Hide Answer"
+            hideHTMLElements(e.target.parentNode.childNodes[1]); //Hide "Submit Answer" button. 
             for(i = 0; i < inputBox.length; i++) { 
-                inputBox[i].value = val[parent.id][i];  
+                inputBox[i].value = val[parent.id][i]; // Insert answers from JSON file to the input fields. 
             }
-        }else { 
-            showHTMLElements(e.target.parentNode.childNodes[1]);
-            e.target.innerHTML = 'Show Answer'; 
+        }else { // If not clicked, 
+            showHTMLElements(e.target.parentNode.childNodes[1]); //Display "Submit Answer" button. 
+            e.target.innerHTML = 'Show Answer'; // Change text of the button from "Hide Answer" to "Show Answer". 
             for(j = 0; j < inputBox.length; j++) { 
-                inputBox[j].value = '';  
+                inputBox[j].value = '';  // Reset all input field to blank. 
             }
         }
     })
 })
 
 
+//Functions
 hideHTMLElements = (element) => { 
     element.style.display = "none"; 
 }
--- css-modules/script/validate-css-exercises.js
+++ css-modules/script/validate-css-exercises.js
@@ -15,91 +15,95 @@ getAnswer().then((data) => {
 });
 
 //Event Listeners
-
-submitBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
-    const inputBox = parent.querySelectorAll("input"); //Query the form --> Grab all the input fields in the form
-    const codeSnippetWrapper = parent.querySelector(".code-snippet-wrapper"); //Retrieve the wrapper to change the contents(i.e if answer is wrong or correct)
-    let flag;
-    if (e.target.className === "try-again") {
-      tryAgain(e.target, parent);
-      showHTMLElements(e.target.parentNode.childNodes[5]);
-    } else {
-      for (i = 0; i < inputBox.length; i++) {
-        flag = true;
-        if (inputBox[i].value.toLowerCase() !== val[parent.id][i]) {
-          //Check input value of user against answer in the JSON file.
-          flag = false; //Sets the a flag to TRUE if each input values are correct.
-          break;
-        }
+submitBtns.forEach(btn => { 
+  btn.addEventListener('click', (e) => { 
+      e.preventDefault();
+      const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
+      const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form (Where each input fields are stored in a nodeList)
+      const codeSnippetWrapper = parent.querySelector('.code-snippet-wrapper'); //Retrieve the wrapper to change the contents(i.e if answer is wrong or correct)  
+      let correct; 
+          
+      if(e.target.className === 'try-again') { // Check if the button was already clicked before. If so, we should allow the user to try again. 
+          tryAgain(e.target, parent); // Fire the function "tryAgain". Takes 2 arguments (the submit button, and the form)
+          showHTMLElements(e.target.parentNode.childNodes[5]); // Show the "Show Answer" button. 
+      }else { 
+          for(i = 0; i < inputBox.length; i++) { // Cycle through all the input fields in the form 
+              correct = true; 
+              if(inputBox[i].value !== val[parent.id][i]){ //Check input value of user against answer in the JSON file. 
+                  correct = false;  // If one answer is wrong, set flag to FALSE and BREAK from the loop. 
+                  break; 
+              }
+          }
+      
+          if(correct){ 
+              //CALL ON THE FUNCTION changeHTMLElements to show appropriate template (i.e --> either wrong template OR correct template). 
+              changeHTMLElements(e.target, codeSnippetWrapper, correct); 
+          }else {
+              changeHTMLElements(e.target, codeSnippetWrapper, correct)
+          }
       }
 
-      if (flag) {
-        //CALL ON THE FUNCTION changeHTMLElements.
-        changeHTMLElements(e.target, codeSnippetWrapper, flag);
-      } else {
-        changeHTMLElements(e.target, codeSnippetWrapper, flag);
+  }) 
+})
+
+
+showAnsBtns.forEach(btn => { 
+  btn.addEventListener('click', e => {    
+      e.preventDefault(); 
+      e.target.classList.toggle('clicked'); // The class of the button will toggle. That is, it will either be "" or clicked.  
+      const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
+      const inputBox = parent.querySelectorAll('input'); //Query the form --> Grab all the input fields in the form
+
+      if(e.target.className !== '') { // Checks if the user clicked on the button. If so, 
+          e.target.innerHTML = 'Hide Answer'; // Change the text inside the button from "Show Answer" to "Hide Answer"
+          hideHTMLElements(e.target.parentNode.childNodes[1]); //Hide "Submit Answer" button. 
+          for(i = 0; i < inputBox.length; i++) { 
+              inputBox[i].value = val[parent.id][i]; // Insert answers from JSON file to the input fields. 
+          }
+      }else { // If not clicked, 
+          showHTMLElements(e.target.parentNode.childNodes[1]); //Display "Submit Answer" button. 
+          e.target.innerHTML = 'Show Answer'; // Change text of the button from "Hide Answer" to "Show Answer". 
+          for(j = 0; j < inputBox.length; j++) { 
+              inputBox[j].value = '';  // Reset all input field to blank. 
+          }
       }
-    }
-  });
-});
+  })
+})
 
-showAnsBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    e.target.classList.toggle("clicked");
-    const parent = e.target.parentNode.parentNode.parentNode; //Grab the parent element (form tag)
-    const inputBox = parent.querySelectorAll("input"); //Query the form --> Grab all the input fields in the form
 
-    if (e.target.className !== "") {
-      e.target.innerHTML = "Hide Answer";
-      hideHTMLElements(e.target.parentNode.childNodes[1]);
-      for (i = 0; i < inputBox.length; i++) {
-        inputBox[i].value = val[parent.id][i];
-      }
-    } else {
-      showHTMLElements(e.target.parentNode.childNodes[1]);
-      e.target.innerHTML = "Show Answer";
-      for (j = 0; j < inputBox.length; j++) {
-        inputBox[j].value = "";
-      }
-    }
-  });
-});
+//Functions
+hideHTMLElements = (element) => { 
+  element.style.display = "none"; 
+}
 
-hideHTMLElements = (element) => {
-  element.style.display = "none";
-};
+showHTMLElements = (element) => { 
+  element.style.display = "block"; 
+}
 
-showHTMLElements = (element) => {
-  element.style.display = "block";
-};
-
-changeHTMLElements = (btn, template, flag) => {
-  hideHTMLElements(template.querySelector(".code-snippet")); //HIDE THE current content in the wrapper.
-  const correctTemplate = template.querySelector(".correct");
-  const wrongTemplate = template.querySelector(".wrong");
-  const btnWrapper = template.querySelector(".btn-wrapper");
-
-  if (flag) {
-    //If flag returns TRUE --> load The check mark and display message 'Correct!'
-    showHTMLElements(correctTemplate);
-    hideHTMLElements(btnWrapper);
-  } else {
-    btn.innerHTML = "Try Again";
-    btn.classList.add("try-again");
-    hideHTMLElements(btnWrapper.childNodes[5]);
-    showHTMLElements(wrongTemplate);
+changeHTMLElements = (btn,template,flag) => { 
+  hideHTMLElements(template.querySelector('.code-snippet')); //HIDE THE current content in the wrapper. 
+  const correctTemplate = template.querySelector('.correct'); 
+  const wrongTemplate = template.querySelector('.wrong'); 
+  const btnWrapper = template.querySelector('.btn-wrapper'); 
+  
+  if(flag){ 
+      //If flag returns TRUE --> load The check mark and display message 'Correct!'
+      showHTMLElements(correctTemplate);
+      hideHTMLElements(btnWrapper); 
+  }else { 
+      btn.innerHTML = 'Try Again'; 
+      btn.classList.add('try-again'); 
+      hideHTMLElements(btnWrapper.childNodes[5]);
+      showHTMLElements(wrongTemplate);
   }
-};
+}
 
-tryAgain = (btn, parent) => {
-  btn.classList.toggle("try-again");
-  const template = parent.querySelector(".code-snippet");
-  const wrongTemplate = parent.querySelector(".wrong");
-  hideHTMLElements(wrongTemplate);
-  showHTMLElements(template);
-  btn.innerHTML = "Submit your answer";
-};
+tryAgain = (btn,parent) => { 
+  btn.classList.toggle('try-again'); 
+  const template = parent.querySelector('.code-snippet');
+  const wrongTemplate = parent.querySelector('.wrong'); 
+  hideHTMLElements(wrongTemplate); 
+  showHTMLElements(template); 
+  btn.innerHTML = 'Submit your answer'; 
+}
+
