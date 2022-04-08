93086d36642cba34d3fb8dc3cf8595b6beb0a2a8
k-faardheen <k.faardheen@gmail.com>
2022-04-08 15:08:08 +0400

    database folder created

---
+++ database design/course.sql
@@ -0,0 +1,46 @@
+--Create table course 
+    CREATE TABLE course (
+        course_id VARCHAR(4) NOT NULL PRIMARY KEY, 
+        name varchar(50)
+    ); 
+
+--Populate the table 
+INSERT INTO course(course_id, name)
+VALUES(
+	'C001', 'Java'
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C002', 'JavaScript'
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C003', 'Python'
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C004', 'HTML'
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C005', 'CSS' 
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C006', 'R'
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C007', 'SQL' 
+); 
+
+INSERT INTO course(course_id, name)
+VALUES(
+	'C008', 'Spring Boot'
+); 
