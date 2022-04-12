4d6d7cb2e596a08a401ec26fefc7f0edb413fba9
k-faardheen <k.faardheen@gmail.com>
2022-04-12 21:05:50 +0400

    created views

--- database design/user access/user access.sql
+++ database design/user access/user access.sql
@@ -1,8 +1,12 @@
 -- CREATE USER
 CREATE USER faardheen; 
 CREATE USER muntaswir; 
+--As admin we have access on the whole database. 
+GRANT ALL PRIVILEGES ON DATABASE lebateau TO faardheen;
+GRANT ALL PRIVILEGES ON DATABASE lebateau TO muntaswir;
 
 -- Contributor 
+-- Suzy and Shawna are both registered as Contributor. 
 CREATE USER Suzy; 
 CREATE USER Shawna; 
 
@@ -11,6 +15,3 @@ GRANT SELECT, INSERT ON course, quiz, answer TO Suzy;
 GRANT SELECT, INSERT ON course, quiz, answer TO Shawna; 
 
 
---As admin we have access on the whole database. 
-GRANT ALL PRIVILEGES ON DATABASE lebateau TO faardheen;
-GRANT ALL PRIVILEGES ON DATABASE lebateau TO muntaswir;
---
+++ database design/views.sql
@@ -0,0 +1,19 @@
+--List the names of the students and the course they are enrolled to. 
+CREATE VIEW student_data AS 
+    SELECT s.student_id, r.first_name, r.last_name, r.email, c.name AS course_name
+    FROM registration r, student s, enrollement e, course c
+    WHERE s.registration_id = r.registration_id 
+    AND s.student_id = e.student_id 
+    AND e.course_id = c.course_id;  
+
+-- Granted view access to SUZY 
+GRANT SELECT ON student_data TO suzy; 
+
+--List the names of the contributor and the number of courses they contributed to. 
+CREATE VIEW contributor_course_contributed AS
+    SELECT cc.contributor_id, r.first_name, r.last_name, r.email, COUNT(cc.course_id) AS Number_Of_Courses_Contributed
+    FROM registration r, contributor c, contributor_course cc
+    WHERE c.registration_id = r.registration_id
+    AND c.contributor_id = cc.contributor_id
+    GROUP BY (cc.contributor_id, r.first_name, r.last_name, r.email)
+    ORDER BY contributor_id; 
